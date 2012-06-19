e7686bc4878e00331f471e95e585e2ca2101d24b
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-06-19 11:57:13 +0100

    compojure dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject workflow-runner "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+(defproject workflow-runner "0.1-SNAPSHOT"
+  :description "Wf4Ever workflow runner service"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                  [compojure-rest "0.2.2"]
+                  [compojure "1.1.0"]]
+  :plugins [[lein-ring "0.7.1"]]
+  :ring {:handler workflow-runner.core/app})
