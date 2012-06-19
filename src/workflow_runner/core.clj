964158e57af167f903398a87210db445641f2dff
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-06-19 11:57:28 +0100

    compojure example

--- src/workflow_runner/core.clj
+++ src/workflow_runner/core.clj
@@ -1,1 +1,12 @@
-(ns workflow-runner.core)
+(ns workflow-runner.core
+  (:use compojure.core)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]))
+
+(defroutes main-routes
+  (GET "/" [] "<h1>Hello World Wide Web!</h1>")
+  (route/resources "/")
+  (route/not-found "Resource not found"))
+
+(def app
+  (handler/site main-routes))
