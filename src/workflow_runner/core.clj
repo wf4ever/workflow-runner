f1190e531e660f080fe5dc567e0ddad0055677f8
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-06-19 11:19:11 +0100

    workflow runner for wf4ever

---
+++ src/workflow_runner/core.clj
@@ -0,0 +1,1 @@
+(ns workflow-runner.core)
---
+++ test/workflow_runner/test/core.clj
@@ -0,0 +1,6 @@
+(ns workflow-runner.test.core
+  (:use [workflow-runner.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
