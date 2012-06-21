faf34e3304b9c65d10026dd336632f0c4fb08025
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-06-21 11:53:03 +0100

    Using correct (is) syntax..

--- test/workflow_runner/test/t2server.clj
+++ test/workflow_runner/test/t2server.clj
@@ -1,6 +1,7 @@
 (ns workflow-runner.test.t2server
   (:use [clojure.java.io :only [resource input-stream]])
   (:use [workflow-runner.t2server])
+  (:use [clj-time.core :only [after? now]])
   (:use [clojure.test]))
 
 (def ^:dynamic *server* "http://sandbox.wf4ever-project.org/taverna-server/rest/")
@@ -8,32 +9,68 @@
 (def ^:dynamic *server-pw* "taverna")
 
 (deftest test-authenticated
-   (let [req (authenticated "fred" "soup")]
-     (is ["fred" "soup"] (:basic-auth req))
-     (is :auto (:as req))))
+  (let [req (authenticated "fred" "soup")]
+     (is (= ["fred" "soup"] (:basic-auth req)))
+     (is (= :auto (:as req)))))
 
 (deftest test-map-value-map
-  (is {:a "1" :b "{}" :c "[]"}
-     (map-value-map str {:a 1 :b {} :c []})))
+  (is (= {:a "1" :b "{}" :c "[]"})
+         (map-value-map str {:a 1 :b {} :c []})))
 
 (deftest test-connect
   (let [s (connect *server* *server-user* *server-pw*)]
-    (is true (map? s))
-    (is *server* (:url s))
-    (is (str *server* "runs/") (:runs s))
-    (is true (map? (::workflow-runner.t2server/req *server*)))))
+    (is (map? s))
+    (is (= *server* (:url s)))
+    (is (= (str *server* "runs/") (:runs s)))
+    (is (map? (::workflow-runner.t2server/req *server*)))))
 
 (deftest test-runs
   (let  [s (connect *server* *server-user* *server-pw*)
          r (runs s)]
-    (is seq? r)
+    (is (seq? r))
     ; Should be a list of URIs
-    (map #(is true (and (string? %) (.startsWith % *server*))) r)))
+    (map #(is (and (string? %) (.startsWith % *server*))) r)))
 
 (deftest test-new-run
   (let [wf (slurp (resource "helloworld.t2flow") :encoding "utf-8")
         s (connect *server* *server-user* *server-pw*)
-        r (new-run s wf)]
-      (is true (string? r))
-      (is true (.startsWith r *server*))))
+        r-uri (new-run s wf)]
+    (is (string? r-uri))
+    (is (.startsWith r-uri *server*))
+    (is (contains? r-uri (runs s)))))
 
+(deftest test-datetime?
+    (is (not (datetime? nil)))
+    (is (not (datetime? (str (now)))))
+    (is (datetime? (now) (now))))
+
+(deftest test-run
+  (let [wf (slurp (resource "helloworld.t2flow") :encoding "utf-8")
+        s (connect *server* *server-user* *server-pw*)
+        r (run s (new-run s wf))]
+    (is (map? r))
+    (is (= r (:url r)))
+    ((map? (::workflow-runner.t2server/req *server*)))
+    (is (.startsWith (:status r)))))
+
+(deftest test-run-get
+  (let [wf (slurp (resource "helloworld.t2flow") :encoding "utf-8")
+        s (connect *server* *server-user* *server-pw*)
+        r (run s (new-run s wf))]
+    (is (= :Initialized (run-get r :status)))
+    (is (after? (run-get r :expiry) (run-get r :createTime)))
+    (is (nil? (run-get r :finishTime)))
+    (is (map? (run-get r :inputs)))
+    (is (nil? (run-get r :somethingUnknown)))))
+
+(deftest test-run-set
+  (let [wf (slurp (resource "helloworld.t2flow") :encoding "utf-8")
+        s (connect *server* *server-user* *server-pw*)
+        r (run s (new-run s wf))
+        created (run-get r :createTime)]
+    (is (= :Finished (run-set r :status :Finished)))
+    (is (= :Finished (run-get r :status)))
+    (println (run-set r :expiry created))
+    (is (= created (run-get r :expiry)))))
+
+ 
